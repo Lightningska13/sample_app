eb9691bedf2abd8856e15c6f64776ae25fa58fe8
Patrick <patrick.lighthall@gmail.com>
2011-06-07 11:03:36 -0400

    Removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  .
-  .
-  .
+  match '/contact' => 'pages#contact'
+  match '/about' => 'pages#about'
+  match '/help' => 'pages#help'
+  
+  root :to => 'pages#home'
 end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+
 
